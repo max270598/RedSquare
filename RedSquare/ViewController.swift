79035b992c4d2ee17ce066963c1cd969d1a77001
Max <8102892@gmail.com>
2021-03-16 19:01:55 +0300

    Initial Commit

---
+++ RedSquare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedSquare
+//
+//  Created by Мах Ol on 3/16/21.
+//  Copyright © 2021 Мах Ol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
